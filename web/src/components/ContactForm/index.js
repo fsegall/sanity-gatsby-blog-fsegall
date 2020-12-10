8d2cfe4b1720fdd414c0e276f06c0dd25a8c2c1c
fsegall <segall.felipe@gmail.com>
2020-12-10 20:45:06 -0300

    Connects form to netlify

--- web/src/components/ContactForm/index.js
+++ web/src/components/ContactForm/index.js
@@ -5,7 +5,7 @@ export default function ContactForm () {
   return (
     <div className={styles.formContainer}>
       <h2 className={styles.headerStyle} >Say hello!</h2>
-      <form className={styles.formStyle} method="post" action="https://getform.io/{your-unique-getform-endpoint}">
+      <form className={styles.formStyle} method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
         <label className={styles.labelStyle} htmlFor="name">
           Name:
           <input className={styles.inputStyle} type="text" name="name" id="name" placeholder='name'/>
@@ -14,6 +14,7 @@ export default function ContactForm () {
           Email:
           <input className={styles.inputStyle} type="email" name="email" id="email" placeholder='email'/>
         </label>
+        <input type="hidden" name="bot-field" />
         <label className={styles.labelStyle}>Leave a message</label>
         <textarea className={styles.textAreaStyle}name="message" id="message" cols="30" rows="10" />
         <button className={styles.submitButton} type="submit">Submit</button>
