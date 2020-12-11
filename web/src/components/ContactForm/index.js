7c32db0ecd485535b4f17192fbc79e824b7a0b9d
fsegall <segall.felipe@gmail.com>
2020-12-10 21:34:57 -0300

    Testing form with hidden input without htmlFor

--- web/src/components/ContactForm/index.js
+++ web/src/components/ContactForm/index.js
@@ -7,17 +7,17 @@ export default function ContactForm () {
       <h2 className={styles.headerStyle} >Say hello!</h2>
       <form className={styles.formStyle} method="POST" netlify-honeypot="bot-field" data-netlify="true" name="contact">
         <input type="hidden" name="form-name" value="contact" />
-        <label className={styles.labelStyle} htmlFor="name">
+        <label className={styles.labelStyle} >
           Name
           <input className={styles.inputStyle} type="text" name="name" id="name" placeholder='name'/>
         </label>
-        <label className={styles.labelStyle} htmlFor="email">
+        <label className={styles.labelStyle} >
           Email
           <input className={styles.inputStyle} type="email" name="email" id="email" placeholder='email'/>
         </label>
         <input type="hidden" name="bot-field" />
         <label className={styles.labelStyle}>Leave a message</label>
-        <textarea className={styles.textAreaStyle} name="message" id="message" cols="30" rows="10" />
+        <textarea className={styles.textAreaStyle} name="message" cols="30" rows="10" />
         <button className={styles.submitButton} type="submit">Submit</button>
       </form>
     </div>
