51801a51e019613b8384294ca9c96961f44cd5ce
fsegall <segall.felipe@gmail.com>
2020-12-10 23:45:09 -0300

    Position burger menu

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -7,29 +7,31 @@ import styles from './header.module.css'
 import Navbar from './Navbar'
 
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
-  <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <div className={styles.branding}>
-        <Link to='/'>{siteTitle}</Link>
-        <Navbar />
+  <>
+    <div className={styles.root}>
+      <div className={styles.wrapper}>
+        <div className={styles.branding}>
+          <Link to='/'>{siteTitle}</Link>
+          <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
+            <div>
+              <Icon symbol='hamburger' />
+            </div>
+          </button>
+          <Navbar />
+        </div>
+        <nav className={cn(styles.nav, showNav && styles.showNav)}>
+          <ul>
+            <li>
+              {/* <Link to='/archive/'>Archive</Link> */}
+              <Link to='/portfolio/'>Portfolio</Link>
+              <Link to='/contact/'>Contact</Link>
+              <Link to='/archive/'>Archive</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
-      </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            {/* <Link to='/archive/'>Archive</Link> */}
-            <Link to='/portfolio/'>Portfolio</Link>
-            <Link to='/contact/'>Contact</Link>
-            <Link to='/archive/'>Archive</Link>
-          </li>
-        </ul>
-      </nav>
     </div>
-  </div>
+  </>
 )
 
 export default Header
