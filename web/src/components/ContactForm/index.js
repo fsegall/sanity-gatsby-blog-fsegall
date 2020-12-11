c43bf3dc331956ecb4e3e4913f2257831d7740e0
fsegall <segall.felipe@gmail.com>
2020-12-10 23:02:15 -0300

    Adds css to form

--- web/src/components/ContactForm/index.js
+++ web/src/components/ContactForm/index.js
@@ -8,15 +8,15 @@ export default function ContactForm () {
       <form className={styles.formStyle} method="POST" netlify-honeypot="bot-field" data-netlify="true" name="contact">
         <input type="hidden" name="form-name" value="contact" />
         <label className={styles.labelStyle} >
-          Name
+          <span>Name</span>
           <input className={styles.inputStyle} type="text" name="name" id="name" placeholder='name'/>
         </label>
         <label className={styles.labelStyle} >
-          Email
+          <span>Email</span>
           <input className={styles.inputStyle} type="email" name="email" id="email" placeholder='email'/>
         </label>
         <input type="hidden" name="bot-field" />
-        <label className={styles.labelStyle}>Leave a message</label>
+        <label className={styles.labelStyle}><span>Leave a message</span></label>
         <textarea className={styles.textAreaStyle} name="message" cols="30" rows="10" />
         <button className={styles.submitButton} type="submit">Submit</button>
       </form>
